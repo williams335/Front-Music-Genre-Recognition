f694aa5025d4e00a682b6d3eed128f40594aef96
williams335 <doodzerj@gmail.com>
2021-01-22 14:24:56 +0100

    Responsive v4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'front-music-genre-recognition';
+  title = 'Front Music Genre Recognition';
   isActive = false;
   constructor(private router: Router) { }
 
